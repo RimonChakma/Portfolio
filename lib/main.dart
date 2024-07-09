e191140de1cc8ff3550a46b80149e218418f98b9
RimonChakma <rimon.chakma880@gmail.com>
2024-07-09 19:34:08 +0600

    go to home message

--- lib/main.dart
+++ lib/main.dart
@@ -12,7 +12,7 @@ class MainApp extends StatelessWidget {
     return const MaterialApp(
       home: Scaffold(
         body: Center(
-          child: Text('Our Class is done.'),
+          child: Text('Our Class is done.\nWe will go to our home.'),
         ),
       ),
     );
